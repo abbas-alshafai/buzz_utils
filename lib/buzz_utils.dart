0b0f2b670590e68dadace5ce3036dc169e039b44
abbasalshafai <abbas.alshafai@gmail.com>
2022-03-10 22:04:40 -0500

    add function definitions

--- lib/buzz_utils.dart
+++ lib/buzz_utils.dart
@@ -6,3 +6,4 @@ export 'src/utils/map_utils.dart';
 export 'src/extensions/map_extensions.dart';
 export 'src/extensions/list_extensions.dart';
 export 'src/extensions/string_extensions.dart';
+export 'src/extensions/function_definitions.dart';
