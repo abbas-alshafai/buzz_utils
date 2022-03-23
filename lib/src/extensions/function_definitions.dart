e94b126ec00407a251fe60f583b7007be0bd0a15
abbasalshafai <abbas.alshafai@gmail.com>
2022-03-23 19:39:52 -0400

    added future function definitions

--- lib/src/extensions/function_definitions.dart
+++ lib/src/extensions/function_definitions.dart
@@ -1,3 +1,6 @@
 typedef SetGetValueCallback<T, E> = T Function(E value);
 typedef SetValueCallback<T> = void Function(T value);
 typedef GetValueCallback<T> = T Function();
+typedef FutureSetGetValueCallback<T, E> = Future<T> Function(E value);
+typedef FutureSetValueCallback<T> = Future<void> Function(T value);
+typedef FutureGetValueCallback<T> = Future<T> Function();
