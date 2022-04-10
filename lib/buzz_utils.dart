b26acee8a59195a67ccf2a7fa5fcae15cb772a02
abbasalshafai <abbas.alshafai@gmail.com>
2022-04-10 11:07:49 -0400

    added color extensions

--- lib/buzz_utils.dart
+++ lib/buzz_utils.dart
@@ -6,4 +6,5 @@ export 'src/utils/map_utils.dart';
 export 'src/extensions/map_extensions.dart';
 export 'src/extensions/list_extensions.dart';
 export 'src/extensions/string_extensions.dart';
+export 'src/extensions/color_extensions.dart';
 export 'src/extensions/function_definitions.dart';
